b33bc38cd2d6e06e0492735584f29393ce5251be
Adam Lopez <adamf04@gmail.com>
2021-05-25 21:07:20 -0400

    Adding functionality for showing the birthday change state when a user clicks a date from the selection box. If you run the app and open the console and click a date, the console will love the birthday and the newDate

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,33 @@ import {Form, FormControl, Button } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            newDate: '',
+            birthday: '1986-07-01'
+        }
+    }
+
+    changeBirthday() {
+        console.log(this.state);
+        this.setState({ birthday: this.state.newDate })
+    }
+
     render() {
         return (
             <div className="App">
                 <Form inline>
                     <h2>Input Your Birthday!</h2>
-                    <FormControl type="date">
+                    <FormControl 
+                        type="date"
+                        onChange={ event => this.setState({ newDate: event.target.value }) }
+                    >
                     </FormControl>
                     {' '}
-                    <Button>
+                    <Button onClick={ () => this.changeBirthday() }>
                         Submit
                     </Button>
                 </Form>
